b1bf577fa508395323a17197fe2c73fedce3fb3e
unknown <tatangamess@gmail.com>
2021-06-22 11:18:13 -0600

    sistema v1.1.2

--- resources/views/frontend/login/login.blade.php
+++ resources/views/frontend/login/login.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
           integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
+    <link href="{{ asset('images/icono-sistema.png') }}" rel="icon">
     <!--Fontawesome CDN-->
     <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
     <!-- comprimido de librerias -->
